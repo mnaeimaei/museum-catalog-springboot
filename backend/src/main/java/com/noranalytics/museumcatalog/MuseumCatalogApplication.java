711e86d3def85fa30c4872ce376934880949f5f4
mnaeimaei <mnaeimaei@users.noreply.github.com>
2026-02-26 05:22:22 +0100

    chore: initialize project structure

---
+++ backend/src/main/java/com/noranalytics/museumcatalog/MuseumCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.noranalytics.museumcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/noranalytics/museumcatalog/MuseumCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noranalytics.museumcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
